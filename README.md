41a68b121c7539d23f40b8a2deeb0330075b0ccf
Naugtur <naugtur@gmail.com>
2017-10-01 19:03:50 +0200

    create first working version

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# aframe-livereload-image
+## aframe-livereload-image
+
+[![Version](http://img.shields.io/npm/v/aframe-livereload-image.svg?style=flat-square)](https://npmjs.org/package/aframe-livereload-image)
+[![License](http://img.shields.io/npm/l/aframe-livereload-image.svg?style=flat-square)](https://npmjs.org/package/aframe-livereload-image)
+
+### Livereload for images used in A-Frame 
+
+> It's a development/design tool
+
+Performs live reloading of a `src` attribute on an entity (makes most sense to use with a-sky)
+
+Optimized for image files. Works with `file://` protocol in FireFox and detects changes by comparing file sizes.
+
+What? Why file size? and why just file size?
+
+1. None of the http headers are available via `file://` protocol so not a lot of other options
+1. Most reasonable format for large files is `jpg` anyway and it will change the file size on content changes
+1. Working on a file:// protocol I noticed this component detects when the image manipulation program starts writing the file and sees it as growing from 0 bytes up.
+1. Yes, I will add checking headers in case you're running on http soon.
+
+Built for [A-Frame](https://aframe.io).
+
+### API
+
+```html
+<a-sky livereload="src: image.jpg"></a-sky>
+```
+
+| Property | Description | Default Value |
+| -------- | ----------- | ------------- |
+| src      | path to image to load and watch            |               |
+| freq      | frequency of checking for the file, in miliseconds           |  500             |
+
+### Installation
+
+#### Browser
+
+Install and use by directly including the [browser files](dist):
+
+```html
+<head>
+  <title>My A-Frame Scene</title>
+  <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+  <script src="https://unpkg.com/aframe-livereload-image/dist/aframe-livereload-image.min.js"></script>
+</head>
+
+<body>
+  <a-scene>
+    <a-sky livereload="src: skybackground.jpg"></a-entity>
+  </a-scene>
+</body>
+```
+
+#### npm
+
+Install via npm:
+
+```bash
+npm install aframe-livereload-image
+```
+
+Then require and use.
+
+```js
+require('aframe');
+require('aframe-livereload-image');
+```
---
+++ index.js
@@ -0,0 +1,90 @@
+/* global AFRAME */
+/* global fetch */
+
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+!(function () {
+  function debounce (func, time) {
+    var pointer;
+    return function () {
+      clearTimeout(pointer);
+      pointer = setTimeout(func, time);
+    };
+  }
+
+  /**
+   * Livereload component for A-Frame.
+   */
+  AFRAME.registerComponent('livereload', {
+    schema: {
+      src: {
+        type: 'string'
+      }
+    },
+
+    /**
+     * Set if component needs multiple instancing.
+     */
+    multiple: false,
+
+    /**
+     * Called once when component is attached. Generally for initial setup.
+     */
+    init: function () {
+      var filename = this.data.src;
+      console.log(this, filename);
+      this.el.setAttribute('src', filename);
+      var previousBuffer = {};
+      var replace = debounce(function () {
+        filename = this.data.src + '?a=' + Math.random().toFixed(8);
+        THREE.Cache.files = {};
+        this.el.setAttribute('src', filename);
+      }.bind(this), 1000);
+      this.data.intervalHandle = setInterval(function () {
+        fetch(this.data.src + '?a=' + Math.random().toFixed(8))
+          .then(function (response) { return response.blob(); })
+          .then(function (buffer) {
+            if (buffer.size > 1000 && (buffer.size !== previousBuffer.size)) {
+              console.log('different', buffer.size);
+              replace();
+            }
+            previousBuffer = buffer;
+          })
+          .catch(console.warn);
+      }.bind(this), ~~(this.data.freq) || 500);
+    },
+
+    /**
+     * Called when component is attached and when component data changes.
+     * Generally modifies the entity based on the data.
+     */
+    update: function (oldData) {},
+
+    /**
+     * Called when a component is removed (e.g., via removeAttribute).
+     * Generally undoes all modifications to the entity.
+     */
+    remove: function () {
+      clearInterval(this.data.intervalHandle);
+    },
+
+    /**
+     * Called on each scene tick.
+     */
+    // tick: function (t) { },
+
+    /**
+     * Called when entity pauses.
+     * Use to stop or remove any dynamic or background behavior such as events.
+     */
+    pause: function () {},
+
+    /**
+     * Called when entity resumes.
+     * Use to continue or add any dynamic or background behavior such as events.
+     */
+    play: function () {}
+  });
+})();
